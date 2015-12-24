2b673040e599fe37bb76b2a5d3ae5f9aa9badb8d
Jan Bajena <bajena3@o2.pl>
2015-12-24 08:20:18 +0100

    Created simple scene

---
+++ Meme Volley/Assets/Scripts/Movement.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+    // Update is called once per frame
+	void Update () {
+        float xPos = gameObject.transform.position.x + .1F * Input.GetAxis("Horizontal");
+        gameObject.transform.position = new Vector3(xPos, -1.5F);
+	}
+}
