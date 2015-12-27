a18992cc2e3f1572e4aa381226400682c9374677
Jan Bajena <bajena3@o2.pl>
2015-12-27 14:52:04 +0100

    Added movement script

--- Meme Volley/Assets/Scripts/Movement.cs
+++ Meme Volley/Assets/Scripts/Movement.cs
@@ -1,10 +1,59 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Movement : MonoBehaviour {
-    // Update is called once per frame
-	void Update () {
-        float xPos = gameObject.transform.position.x + .1F * Input.GetAxis("Horizontal");
-        gameObject.transform.position = new Vector3(xPos, -1.5F);
-	}
+public class Movement : MonoBehaviour
+{
+    public float maxXSpeed = 10f;
+    public float jumpForce = 300f;
+    public Transform groundCheck;
+    public LayerMask whatIsGround;
+
+    bool facingRight = true;
+    float groundRadius = 0.2f;
+    bool grounded = true;
+    
+    private Rigidbody2D _rigidBody2D
+    {
+        get
+        {
+            return GetComponent<Rigidbody2D>();
+        }
+    }
+	
+    void FixedUpdate()
+    {
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+        var xMove = Input.GetAxis("Horizontal");
+
+        
+        if (xMove > 0 && !facingRight || xMove < 0 && facingRight)
+        {
+            Flip();
+        }
+
+        _rigidBody2D.velocity = new Vector2(xMove * maxXSpeed, _rigidBody2D.velocity.y);
+    }
+
+    void Update()
+    {
+        Jump();
+    }
+
+    void Jump()
+    {
+        var yMove = Input.GetAxis("Vertical");
+
+        if (grounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            _rigidBody2D.AddForce(new Vector2(0, jumpForce));
+        }
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
 }
